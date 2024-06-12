0a7fb4a04f1f457dfb3a790e949fd43932821748
yakambram <yakambram.kommu@gmail.com>
2024-06-12 14:00:45 +0530

    Added project

---
+++ src/main/java/com/aI/Interview_questions_with_AI/InterviewQuestionsWithAiApplication.java
@@ -0,0 +1,13 @@
+package com.aI.Interview_questions_with_AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewQuestionsWithAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewQuestionsWithAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aI/Interview_questions_with_AI/InterviewQuestionsWithAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aI.Interview_questions_with_AI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewQuestionsWithAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
